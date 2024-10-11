c07da7feef38ae15092ae10a12f4d37ba5e65f8b
Akhilesh <akhilesh.k@carwale.com>
2024-10-11 15:57:32 +0530

    upgrade net 8

--- Enyim.Caching/Configuration/MemcachedClientConfiguration.cs
+++ Enyim.Caching/Configuration/MemcachedClientConfiguration.cs
@@ -147,9 +147,6 @@ namespace Enyim.Caching.Configuration
             {
                 try
                 {
-                    if (options.Transcoder == "BinaryFormatterTranscoder")
-                        options.Transcoder = "Enyim.Caching.Memcached.Transcoders.BinaryFormatterTranscoder";
-
                     var transcoderType = Type.GetType(options.Transcoder);
                     if (transcoderType != null)
                     {
